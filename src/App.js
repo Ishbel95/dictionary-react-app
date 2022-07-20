b772fbbe0b8b770b4d6290048ce3d23f47cf9f1d
Ishbel95 <ishbel95@hotmail.com>
2022-07-20 15:13:31 +0100

    imported axios, added dictionary API integration, fixed deploy bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import React from "react";
 
 export default function App() {
   return (
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,37 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
   function updateKeyword(event) {
     setKeyword(event.target.value);
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={updateKeyword} />
       </form>
+      <div className="container">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+      </div>
     </div>
   );
 }
